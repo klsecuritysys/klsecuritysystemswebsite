afe5e8eed7fdbd82ae04a9d10a17fb742d7ab612
klsecuritysys <87419765+klsecuritysys@users.noreply.github.com>
2021-07-14 13:45:34 +0300

    Update index.vue

--- bihogobuilder-master/pages/index.vue
+++ bihogobuilder-master/pages/index.vue
@@ -9,28 +9,24 @@
       sm8
       md6
     >
-      <span>Chwezxi Procument and Logistics</span>
+      <span>kl security systems </span>
       <br />
-      <builderlanded></builderlanded>
+     
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import builderlanded from '~/components/landed/builderlanded.vue'
-
 
 export default {
-  components: {
-    builderlanded,
-  },
+  
   data() {
        return {
          structuredData: {
                 "@context": "https://schema.org",
             "@type": "WebSite",
-            "url": "https://bihogoo.com",
-            "name": "bihogobusinessbuilder",
+            "url": "https://klsecuritysystems.com",
+            "name": "klsecuritysystems",
          },
        }
      },
@@ -38,23 +34,23 @@ export default {
        return {
          __dangerouslyDisableSanitizers: ['script'],
          script: [{ innerHTML: JSON.stringify(this.structuredData), type: 'application/ld+json' }],
-         title: 'Bihogo Builder : Business building software and Business Plan Software with Result tracking',
+         title: 'klsecurity systems',
            meta: [
 
            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-           { hid: 'keywords', name: 'keywords',content: 'african houses, real estate africa, cheap rentals, Africa 30 day Stays, African interior Design, Laraa Africa Stays, Condo Space Africa, lol, humor, pay with bitcoin, comment, share, laraa, laraa.africa, Condos' },
-           { hid: 'description', name: 'description', content: 'Most African small businesses use BihogooBuilder for business planning, forecasting and financial management. Visit  BihogoBuilder and grow 30% faster.' },
+           { hid: 'keywords', name: 'keywords',content: 'klsecurity systems' },
+           { hid: 'description', name: 'description', content: 'klsecurity systems.' },
            { property:'og:locale' , content:'af-ZA' },
            { property:'og:type' ,content:'website' },
-           { property:'og:title' ,content:'Bihogo Business Builder:  Business Building Software With Performanve Tracking and Extra addons.' },
-           { property: 'og:description' ,content:' Most African Small businesses use BihogooBuilder for business planning, forecasting and financial management. Visit  BihogoBuilder and grow 30% faster.' },
-           { property:'og:site_name' ,content:'bihogoo' },
+           { property:'og:title' ,content:'klsecurity systems' },
+           { property: 'og:description' ,content:' klsecurity systems' },
+           { property:'og:site_name' ,content:'klsecurity systems' },
            { property:'og:url' ,content: 'https://bihogoo.com/'},
            {name:'twitter:card' ,content: 'summary_large_card'},
-           {name:'twitter:description', content:' Most African Small businesses use BihogooBuilder for business planning, forecasting and financial management. Visit  BihogoBuilder and grow 30% faster.'},
+           {name:'twitter:description', content:' klsecurity systems'},
            { name:'twitter:creator' ,content: '@bihogoo.com'},
            {name: 'twitter:site', content: '@bihogo_partners'},
-           { name:'twitter:title', content:'Bihogo Builder : Busienss building software and Business Plan Software with Result tracking and extra addons'},
+           { name:'twitter:title', content:'klsecurity systems'},
            { name:'twitter:image', content:'https://source.unsplash.com/CiMITAJtb6I'},
          ],
        }
