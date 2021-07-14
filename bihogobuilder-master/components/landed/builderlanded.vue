b50074571b3bd88f683e17a8de1a4bf6073979c5
klsecuritysys <87419765+klsecuritysys@users.noreply.github.com>
2021-07-14 13:38:39 +0300

    Add files via upload
    clone

---
+++ bihogobuilder-master/components/landed/builderlanded.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <span>Time for International Markets to feel Africa Vibration and Creative Problem Solving Arts</span>
+      <br />
+      <span>Chwezxi Procurement Logistics</span>
+      <br />
+      <br />
+      <span>Get Your African Product Delivered Anyhwere in the world with precision and sustainability</span>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  name:"builder landed",
+    data: () => ({
+      count: 0,
+    }),
+  }
+</script>
